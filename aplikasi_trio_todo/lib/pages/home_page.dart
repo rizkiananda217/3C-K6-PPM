0bd66ff124fcaeed25d5980f51775bcad4b98eaa
IvanFadil <ivanf222003@gmail.com>
2023-12-24 00:13:17 +0700

    menambahkan fitur tanggal hari ini sama hari hari berikutnya dengan horizontal

--- aplikasi_trio_todo/lib/pages/home_page.dart
+++ aplikasi_trio_todo/lib/pages/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:date_picker_timeline/date_picker_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:aplikasi_trio_todo/pages/theme.dart';
 import 'package:aplikasi_trio_todo/services/theme_services.dart';
@@ -13,37 +15,71 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  DateTime _selectedData = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
       body: Column(
         children: [
+          _addTaskBar(),
+          _addDateBar(),
+        ],
+      ),
+    );
+  }
+
+  _addDateBar() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20, left: 20),
+      child: DatePicker(
+        DateTime.now(),
+        height: 100,
+        width: 80,
+        initialSelectedDate: DateTime.now(),
+        selectionColor: primaryClr,
+        selectedTextColor: Colors.white,
+        dateTextStyle: GoogleFonts.lato(
+          textStyle: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.w600, color: Colors.grey),
+        ),
+        dayTextStyle: GoogleFonts.lato(
+          textStyle: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.w600, color: Colors.grey),
+        ),
+        monthTextStyle: GoogleFonts.lato(
+          textStyle: TextStyle(
+              fontSize: 14, fontWeight: FontWeight.w600, color: Colors.grey),
+        ),
+        onDateChange: (date) {
+          _selectedData = date;
+        },
+      ),
+    );
+  }
+
+  _addTaskBar() {
+    return Container(
+      margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
           Container(
-            margin: const EdgeInsets.only(left: 20, right: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  margin: const EdgeInsets.only(left: 20, right: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        DateFormat.yMMMMd().format(DateTime.now()),
-                        style: subHeadingStyle,
-                      ),
-                      Text(
-                        "Today",
-                        style: headingStyle,
-                      )
-                    ],
-                  ),
+                Text(
+                  DateFormat.yMMMMd().format(DateTime.now()),
+                  style: subHeadingStyle,
                 ),
-                MyButton(label: " \n  + AddTask", onTap: () => null)
+                Text(
+                  "Today",
+                  style: headingStyle,
+                )
               ],
             ),
-          )
+          ),
+          MyButton(label: " \n    + AddTask", onTap: () => null)
         ],
       ),
     );
